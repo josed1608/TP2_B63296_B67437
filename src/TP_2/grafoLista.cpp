688e92d221ba330d4ebf45cbc77f4f01367274e4
Jose David Vargas <jose1608@gmail.com>
2017-11-05 15:41:11 -0600

    Finished grafoLista, but it hasn't been tested

--- src/TP_2/grafoLista.cpp
+++ src/TP_2/grafoLista.cpp
@@ -2,7 +2,137 @@
 
 vert vertNulo = nullptr;
 
-Grafo::Grafo()
-{
+Grafo::Grafo():
+	nVerts(0),
+	nAristas(0),
+	primero(nullptr)
+{}
 
+Grafo::~Grafo()
+{
+	this->vaciar();
+}
+
+void Grafo::vaciar()
+{
+	NodoPrincipal* vertice = primero;
+	while(vertice != nullptr)
+	{
+		NodoSecundario* arista = vertice->primerAdy;
+		while(arista != nullptr)
+		{
+			NodoSecundario* borrable = arista;
+			arista = arista->siguienteAdy;
+			delete borrable;
+		}
+		NodoPrincipal* borrable = vertice;
+		vertice = vertice->siguienteVert;
+		delete borrable;
+	}
+}
+
+NodoPrincipal *Grafo::agregarVert(std::string etiq)
+{
+	primero = new NodoPrincipal(etiq, primero);
+	nVerts++;
+	return primero;
+}
+
+void Grafo::agregarArista(NodoPrincipal *v1, NodoPrincipal *v2, int peso)
+{
+	v1->primerAdy = new NodoSecundario(peso, v2, v1->primerAdy);
+	nAristas++;
+}
+
+void Grafo::modificarPeso(NodoPrincipal *v1, NodoPrincipal *v2, int peso)
+{
+	NodoSecundario* arista = v1->primerAdy;
+	while(arista != nullptr && arista->refListaPrincipal != v2)
+	{
+		arista = arista->siguienteAdy;
+	}
+	arista->peso = peso;
+}
+
+void Grafo::eliminarVert(NodoPrincipal *v)
+{
+	if(primero == v)
+	{
+		primero = primero->siguienteVert;
+	}
+	else
+	{
+		NodoPrincipal* vertice = primero;
+		while(vertice->siguienteVert != v)
+		{
+			vertice = vertice->siguienteVert;
+		}
+		vertice->siguienteVert = v->siguienteVert;
+	}
+	delete v;
+	nVerts--;
+}
+
+void Grafo::eliminarArista(NodoPrincipal *v1, NodoPrincipal *v2)
+{
+	if(v1->primerAdy->refListaPrincipal == v2)
+	{
+		NodoSecundario* borrable = v1->primerAdy;
+		v1->primerAdy = v1->primerAdy->siguienteAdy;
+		delete borrable;
+	}
+	else
+	{
+		NodoSecundario* arista = v1->primerAdy;
+		while(arista != nullptr && arista->siguienteAdy->refListaPrincipal != v2)
+		{
+			arista = arista->siguienteAdy;
+		}
+		NodoSecundario* borrable = arista->siguienteAdy;
+		arista->siguienteAdy = arista->siguienteAdy->siguienteAdy;
+		delete borrable;
+	}
+	nAristas--;
+}
+
+void Grafo::modificarEtiq(NodoPrincipal *v, std::string etiq)
+{
+	v->etiqueta = etiq;
+}
+
+int Grafo::numAristasSalida(NodoPrincipal *v)
+{
+	int contador = 0;
+	NodoSecundario* arista = v->primerAdy;
+	while(arista != nullptr)
+	{
+		++contador;
+		arista = arista->siguienteAdy;
+	}
+	return contador;
+}
+
+bool Grafo::estaAislado(NodoPrincipal *v)
+{
+	return this->numAristasSalida(v) == 0;
+}
+
+int Grafo::pesoArista(NodoPrincipal *v1, NodoPrincipal *v2)
+{
+	NodoSecundario* arista = v1->primerAdy;
+	while(arista != nullptr && arista->refListaPrincipal != v2)
+	{
+		arista = arista->siguienteAdy;
+	}
+	return arista->peso;
+}
+
+NodoPrincipal *Grafo::steVertAdy(NodoPrincipal *deV1, NodoPrincipal *trasV2)
+{
+	NodoSecundario* arista = deV1->primerAdy;
+	while(arista != nullptr && arista->refListaPrincipal != trasV2)
+	{
+		arista = arista->siguienteAdy;
+	}
+	return arista->siguienteAdy->refListaPrincipal;
 }
--- src/TP_2/grafoLista.h
+++ src/TP_2/grafoLista.h
@@ -20,13 +20,13 @@ struct NodoPrincipal {
 
 ///@brief Nodo para la lista secundaria
 struct NodoSecundario{
+	int peso;
 	NodoPrincipal * refListaPrincipal;
 	NodoSecundario * siguienteAdy;
-	int peso;
-	NodoSecundario(NodoPrincipal * refListaPrincipal= nullptr, NodoSecundario * siguienteAdy=nullptr, int peso = 0):
+	NodoSecundario(int peso = 0, NodoPrincipal * refListaPrincipal= nullptr, NodoSecundario * siguienteAdy=nullptr):
+		peso(peso),
 		refListaPrincipal(refListaPrincipal),
-		siguienteAdy(siguienteAdy),
-		peso(peso)
+		siguienteAdy(siguienteAdy)
 	{
 	}
 
@@ -38,7 +38,50 @@ extern vert vertNulo;
 class Grafo
 {
 public:
+	int nVerts;
+	int nAristas;
+	NodoPrincipal* primero;
+
+public:
 	Grafo();
+
+	~Grafo();
+
+	void vaciar();
+
+	inline bool vacio() {return nVerts==0;}
+
+	NodoPrincipal* agregarVert(std::string etiq);
+
+	void agregarArista(NodoPrincipal* v1, NodoPrincipal* v2, int peso);
+
+	void modificarPeso(NodoPrincipal* v1, NodoPrincipal* v2, int peso);
+
+	void eliminarVert(NodoPrincipal* v);
+
+	void eliminarArista(NodoPrincipal* v1, NodoPrincipal* v2);
+
+	void modificarEtiq(NodoPrincipal* v, std::string etiq);
+
+	inline int numAristas() {return nAristas;}
+
+	inline int numVerts() {return nVerts;}
+
+	int numAristasSalida(NodoPrincipal* v);
+
+	bool estaAislado(NodoPrincipal* v);
+
+	inline std::string etiqueta(NodoPrincipal* v) {return v->etiqueta;}
+
+	int pesoArista(NodoPrincipal* v1, NodoPrincipal* v2);
+
+	inline NodoPrincipal* primerVertAdy(NodoPrincipal* v) {return v->primerAdy->refListaPrincipal;}
+
+	NodoPrincipal* steVertAdy(NodoPrincipal* deV1, NodoPrincipal* trasV2);
+
+	inline NodoPrincipal* primerVert() {return primero;}
+
+	inline NodoPrincipal* steVert(NodoPrincipal* trasV) {return trasV->siguienteVert;}
 };
 
 #endif // GRAFOLISTA_H

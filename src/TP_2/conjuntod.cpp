898274e33989b1e6e554cc74bdde30d94f209359
Jose David Vargas <jose1608@gmail.com>
2017-11-09 08:50:54 -0600

    Added conjunto

--- src/TP_2/conjuntod.cpp
+++ src/TP_2/conjuntod.cpp
@@ -1,6 +1,125 @@
 #include "conjuntod.h"
 
-ConjuntoD::ConjuntoD()
-{
+ConjuntoD::ConjuntoD():
+	primero(nullptr)
+{}
 
+ConjuntoD::~ConjuntoD()
+{
+	vaciar();
+}
+
+void ConjuntoD::vaciar()
+{
+	NodoConjunto* c = primero;
+	while(c != nullptr)
+	{
+		NodoElemento* e = primero->primerElem;
+		while(e != nullptr)
+		{
+			NodoElemento* borrable = e;
+			e = e->siguienteElemento;
+			delete borrable;
+		}
+		NodoConjunto* borrable = c;
+		c = c->siguienteConjunto;
+		delete borrable;
+	}
+}
+
+bool ConjuntoD::vacio()
+{
+	return (primero == nullptr);
+}
+
+std::string ConjuntoD::conjuntoAlQuePertenece(vert elem)
+{
+	NodoConjunto* c = primero;
+	while(c != nullptr)
+	{
+		NodoElemento* e = primero->primerElem;
+		while(e != nullptr)
+		{
+			if(e->vertice == elem)
+			{
+				return c->etiqueta;
+			}
+			e = e->siguienteElemento;
+		}
+		c = c->siguienteConjunto;
+	}
+	return std::string();
+}
+
+std::string ConjuntoD::agregarConjunto(std::string etiqueta, vert elem)
+{
+	primero = new NodoConjunto(etiqueta, primero, new NodoElemento(elem));
+	return etiqueta;
+}
+
+std::string ConjuntoD::unirConjuntos(std::string c1, std::string c2)
+{
+	//Encuentro los conjuntos en la lista principal
+	NodoConjunto* conjunto1 = primero;
+	NodoConjunto* conjunto2 = primero;
+	NodoConjunto* anteriorC2 = nullptr; //para borrar a conjunto2
+	bool c1Encontrado = false, c2Encontrado = false;
+	while(!c1Encontrado && !c2Encontrado)
+	{
+		if(conjunto1->etiqueta == c1)
+		{
+			c1Encontrado = true;
+		}
+		else if(conjunto2->etiqueta == c2)
+		{
+			c2Encontrado = true;
+		}
+
+		if(c1Encontrado && !c2Encontrado)
+		{
+			anteriorC2 = conjunto2;
+			conjunto2 = conjunto2->siguienteConjunto;
+		}
+		else if(c2Encontrado && !c1Encontrado)
+		{
+			conjunto1 = conjunto1->siguienteConjunto;
+		}
+		else
+		{
+			anteriorC2 = conjunto2;
+			conjunto2 = conjunto2->siguienteConjunto;
+			conjunto1 = conjunto1->siguienteConjunto;
+		}
+	}
+
+	//Copio la lista del segundo conjunto en el primero
+	NodoElemento* elem2 = conjunto2->primerElem;
+	while(elem2 != nullptr)
+	{
+		NodoElemento* temp = elem2->siguienteElemento;
+		elem2->siguienteElemento = conjunto1->primerElem;
+		conjunto1->primerElem = elem2;
+		elem2 = temp;
+	}
+
+	//Borro a c2 de la lista principal
+	if(anteriorC2 == nullptr)
+	{
+		primero = primero->siguienteConjunto;
+	}
+	else
+	{
+		anteriorC2->siguienteConjunto = conjunto2->siguienteConjunto;
+	}
+	NodoConjunto* borrable = conjunto2;
+	NodoElemento* e = borrable->primerElem;
+	while(e != nullptr)
+	{
+		NodoElemento* borrable = e;
+		e = e->siguienteElemento;
+		delete borrable;
+	}
+	delete borrable;
+
+	return conjunto1->etiqueta;
 }
--- src/TP_2/conjuntod.h
+++ src/TP_2/conjuntod.h
@@ -1,11 +1,57 @@
 #ifndef CONJUNTOD_H
 #define CONJUNTOD_H
 
+#include "grafoLista.h"
+//#include "grafoMatriz.h"
+
+#include<string>
 
 class ConjuntoD
 {
 public:
+	struct NodoElemento;
+
+	struct NodoConjunto {
+		std::string etiqueta;
+		NodoConjunto * siguienteConjunto;
+		NodoElemento * primerElem;
+		NodoConjunto(std::string etiqueta, NodoConjunto * siguienteConjunto =nullptr, NodoElemento * primerElem= nullptr):
+			etiqueta (etiqueta),
+			siguienteConjunto(siguienteConjunto),
+			primerElem(primerElem)
+		{
+		}
+	};
+
+	///@brief Nodo para la lista secundaria
+	struct NodoElemento{
+		vert vertice;
+		NodoElemento * siguienteElemento;
+		NodoElemento(vert vertice = vertNulo, NodoElemento * siguienteElemento=nullptr):
+			vertice(vertice),
+			siguienteElemento(siguienteElemento)
+		{
+		}
+	};
+
+public:
+	NodoConjunto* primero;
+
+public:
 	ConjuntoD();
+
+	~ConjuntoD();
+
+	void vaciar();
+
+	bool vacio();
+
+	std::string conjuntoAlQuePertenece(vert elem);
+
+	std::string agregarConjunto(std::string etiqueta, vert elem);
+
+	std::string unirConjuntos(std::string c1, std::string c2);
+
 };
 
 #endif // CONJUNTOD_H
